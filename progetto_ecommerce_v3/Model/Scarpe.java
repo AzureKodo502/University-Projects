30fcf5010c649867ec3f704ef933d244d1ff776d
AzureKodo502 <bevtsik98@gmail.com>
2026-02-04 16:40:40 +0100

    Delete progetto_ecommerce_v3/Model directory

--- progetto_ecommerce_v3/Model/Scarpe.java
+++
@@ -1,8 +0,0 @@
-package com.example.progetto_ecommerce_v3.Model;
-
-public record Scarpe (String Id, String Nome, String Prezzo, String Descrizione, String Marchio, String Taglia, String Modello){
-    @Override
-    public String toString(){
-        return Id + ";" + Nome + ";" + Prezzo + ";" + Descrizione + ";" + Marchio + ";"+ Taglia + ";" + Modello + ";";
-    }
-}
--- progetto_ecommerce_v3/Model/Utente.java
+++
@@ -1,6 +0,0 @@
-package com.example.progetto_ecommerce_v3.Model;
-
-public record Utente (String Nome, String Cognome, String Email){
-    @Override
-    public String toString(){return Nome + ";" + Cognome + ";" + Email;}
-}

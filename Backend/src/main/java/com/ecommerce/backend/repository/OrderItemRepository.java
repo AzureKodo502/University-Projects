a03647a5c1ba72e8b1900af17c5fb7f3665d519d
AzureKodo502 <bevtsik98@gmail.com>
2026-02-04 17:05:53 +0100

    Delete Backend directory

--- Backend/src/main/java/com/ecommerce/backend/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.backend.repository;
-
-import com.ecommerce.backend.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
